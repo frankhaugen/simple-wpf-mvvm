f4dae5d666bd4446bf513173f0e4c90800494b45
frank <frank.haugen@gmail.com>
2018-10-29 13:44:34 +0100

    Created very simple outline of project

---
+++ simple-wpf-mvvm/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_wpf_mvvm.Models
+{
+	public static partial class Data
+	{
+		public static string CollectJson()
+		{
+			return File.ReadAllText("Assets/Data/demo.json");
+		}
+
+
+	}
+}
---
+++ simple-wpf-mvvm/ViewModels/Presenter.cs
@@ -0,0 +1,32 @@
+﻿using MinimalMVVM.ViewModels;
+using simple_wpf_mvvm.Models;
+using System.Windows.Input;
+
+namespace simple_wpf_mvvm.ViewModels
+{
+	public class Presenter : ObservableObject
+	{
+		public string _JsonText { get; set; }
+
+		public string JsonText
+		{
+			get { return _JsonText; }
+			set
+			{
+				_JsonText = value;
+				RaisePropertyChangedEvent("JsonText");
+			}
+		}
+
+		public ICommand CollectJsonData
+		{
+			get { return new DelegateCommand(Test); }
+
+		}
+
+		private void Test()
+		{
+			JsonText = Data.CollectJson();
+		}
+	}
+}
